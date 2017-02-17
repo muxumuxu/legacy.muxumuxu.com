b53ba7501c65affe6a8a39214ef1990d98b9959e
Luc Chaissac <hello@lucchaissac.com>
2017-02-17 14:17:52 +0100

    Console log job offer

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,3 +6,7 @@ function myFunction() {
         x.className = "menu-items";
     }
 }
+
+console.log("%cSUPER GREEN! 〠", "color:#33cc33; font-size:60px; font-family: arial; font-weight: bold;");
+console.log("%cWe are always open-minded to new talent, and hungry for highly motivated people to join our team.", "color:black; font-size:120px; font-family: arial; font-weight: bold;");
+console.log("%cJOIN USSSSS -> %chttps://muxumuxu.com/careers/", "color:black; font-size:30px; font-family: arial; font-weight: bold;", "color:blue; font-size:30px; font-family: arial; font-weight: bold;");
