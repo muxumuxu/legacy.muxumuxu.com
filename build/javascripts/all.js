0eec12ba9dfb47ee88c249db3ff1f54e5c1fc644
Luc Chaissac <hello@lucchaissac.com>
2017-02-18 22:12:29 +0100

    build for github pages

---
+++ build/javascripts/all.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "menu-items") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu-items";
+    }
+}
+
+console.log("%cSUPER GREEN! 〠", "color:#33cc33; font-size:60px; font-family: arial; font-weight: bold;");
+console.log("%cWe are always open-minded to new talent, and hungry for highly motivated people to join our team.", "color:black; font-size:120px; font-family: arial; font-weight: bold;");
+console.log("%cJOIN USSSSS -> %chttps://muxumuxu.com/careers/", "color:black; font-size:30px; font-family: arial; font-weight: bold;", "color:blue; font-size:30px; font-family: arial; font-weight: bold;");
