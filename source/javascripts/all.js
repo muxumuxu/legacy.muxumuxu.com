7a5e2d29f4a1a8dab64c17111c82fd4f430ed928
Luc Chaissac <hello@lucchaissac.com>
2017-04-06 23:32:58 +0200

    Console log design

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -29,6 +29,6 @@ $(document).ready(function() {
 });
 
 
-console.log("%cSUPER GREEN! 〠", "color:#33cc33; font-size:60px; font-family: arial; font-weight: bold;");
-console.log("%cWe are always open-minded to new talent, and hungry for highly motivated people to join our team.", "color:black; font-size:120px; font-family: arial; font-weight: bold;");
-console.log("%cJOIN USSSSS -> %chttps://muxumuxu.com/careers/", "color:black; font-size:30px; font-family: arial; font-weight: bold;", "color:blue; font-size:30px; font-family: arial; font-weight: bold;");
+console.log("%c✨💚 SUPER GREEN! 💚✨", "color:#33cc33; font-size:20px; font-family: arial; font-weight: bold;");
+console.log("%cWe are always open-minded to new talent, and hungry for highly motivated people to join our team.", "color:black; font-size:20px; font-family: arial; font-weight: bold;");
+console.log("%cJOIN USSSSS -> %chttps://muxumuxu.com/careers/", "color:black; font-size:20px; font-family: arial; font-weight: bold;", "color:blue; font-size:20px; font-family: arial; font-weight: bold;");
